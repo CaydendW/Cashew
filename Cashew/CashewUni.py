b4d91b02de94f289fd55ab610e7aa969f52b9b38
Cayden <cayden@4dewits.com>
2020-06-06 16:36:58 +0200

    Added license to code

--- Cashew/CashewUni.py
+++ Cashew/CashewUni.py
@@ -1,3 +1,6 @@
+#This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License. To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/.
+#Please credit me at this URL: https://github.com/CaydendW/
+
 import speech_recognition as sr
 import webbrowser
 import wikipedia
